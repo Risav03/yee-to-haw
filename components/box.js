159737e4f06f779fda66c6e3609a9d89b9aba2de
Risav03 <needleweed03@gmail.com>
2023-11-22 15:00:27 +0530

    init

---
+++ components/box.js
@@ -0,0 +1,22 @@
+export default function Box(){
+    return (
+        <div className="text-center bg-white text-black min-[801px]:w-[70%] w-[90%] my-auto">
+            <h1 className="min-[801px]:text-[6vw] text-[13vw] font-extrabold">
+                YOU GAINED
+            </h1>
+            <div>
+                <h2 className="font-bold min-[801px]:text-[4vw] text-[8vw]">
+                    40201%
+                </h2>
+                <h3 className="font-semibold min-[801px]:text-[3vw] text-[6vw]">
+                    40.2x + 5.12 ETH
+                </h3>
+            </div>
+
+            <h3 className="min-[801px]:text-[3.5vw] text-[8vw] font-bold">YEE TO YOUR HAW</h3>
+            <h4>
+
+            </h4>
+        </div>
+    )
+}
